ab0d3191fc67f63cbc4ffbae7fb6f4141603a969
ybf34 <ybenfekih@gmail.com>
2025-04-07 21:08:20 +0200

    refactor: refactor sales snapshot repository
    Signed-off-by: ybf34 <ybenfekih@gmail.com>

--- apps/api/src/main/java/com/dofuspulse/api/repository/ItemSalesSnapshotRepository.java
+++ apps/api/src/main/java/com/dofuspulse/api/repository/ItemSalesSnapshotRepository.java
@@ -2,7 +2,6 @@ package com.dofuspulse.api.repository;
 
 import com.dofuspulse.api.model.ItemSalesSnapshot;
 import com.dofuspulse.api.projections.ItemPrice;
-import com.dofuspulse.api.projections.PriceHistory;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,17 +10,18 @@ import org.springframework.data.repository.query.Param;
 
 public interface ItemSalesSnapshotRepository extends JpaRepository<ItemSalesSnapshot, Long> {
 
-  @Query(value = "SELECT DISTINCT ON (s.snapshot_date) s.snapshot_date AS snapshotDate, "
-      + "       s.prices " + "FROM item_sales_snapshots s " + "WHERE s.item_id = :id "
-      + "AND s.snapshot_date BETWEEN :startDate AND :endDate "
-      + "ORDER BY s.snapshot_date, s.prices[1]", nativeQuery = true)
-  List<PriceHistory> getItemPriceHistoryInDateRange(
-      Long id,
-      LocalDate startDate,
-      LocalDate endDate);
+  @Query(value =
+      "SELECT DISTINCT ON (s.snapshot_date, s.item_id) s.snapshot_date AS snapshotDate, s.item_id, "
+          + " s.prices " + "FROM item_sales_snapshots s " + "WHERE s.item_id IN (:itemIds) "
+          + "AND s.snapshot_date BETWEEN :startDate AND :endDate "
+          + "ORDER BY s.snapshot_date, s.item_id,s.prices[1]", nativeQuery = true)
+  List<ItemPrice> getPriceHistoryInDateRangeForItems(
+      @Param("itemIds") List<Long> itemIds,
+      @Param("startDate") LocalDate startDate,
+      @Param("endDate") LocalDate endDate);
 
-  <T> List<T> findAllByItemIdAndSnapshotDateIsBetween(
-      Long itemId,
+  <T> List<T> findAllByItemIdInAndSnapshotDateIsBetween(
+      List<Long> itemId,
       LocalDate startDate,
       LocalDate endDate,
       Class<T> type);
