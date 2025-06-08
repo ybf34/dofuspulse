916ffdac16763cf2763db7156113b34951e75fc7
ybf34 <ybenfekih@gmail.com>
2025-06-08 14:08:58 +0200

    feat(web): add shadcn dep, init main layout, loading state, sidebar , dark/ligh mode
    Signed-off-by: ybf34 <ybenfekih@gmail.com>

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,20 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
+import Layout from './components/layout/layout.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <App/>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Layout>
+          <QueryClientProvider client={queryClient}>
+            <App/>
+          </QueryClientProvider>
+        </Layout>
+      </ThemeProvider>
     </StrictMode>,
 )
