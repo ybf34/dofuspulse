740b77615353b225cdce263dd5ee21dc67258bd1
ybf34 <ybenfekih@gmail.com>
2025-06-09 02:08:01 +0200

    feat(web): add gearset component, enhance layout,add ui molecules

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import LoadingSpinner from "@/components/loading-spinner.tsx";
+import {ModeToggle} from "@/components/mode-toggle.tsx";
+import Component from "@/components/gear-set.tsx";
 
 function App() {
 
-
   return (
       <div>
+        <ModeToggle/>
         <LoadingSpinner/>
+        <Component/>
       </div>
   )
 }
 
 export default App
+
--- apps/web/src/components/layout/layout.tsx
+++ apps/web/src/components/layout/layout.tsx
@@ -1,14 +1,17 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import {SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {AppSidebar} from "@/components/app-sidebar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({children}: { children: React.ReactNode }) {
   return (
       <SidebarProvider>
-        <AppSidebar />
-        <main>
-          <SidebarTrigger />
-          {children}
-        </main>
+        <div className="flex min-h-screen w-full">
+          <AppSidebar/>
+
+          <main className="flex-1">
+            <SidebarTrigger/>
+            {children}
+          </main>
+        </div>
       </SidebarProvider>
-  )
+  );
 }
